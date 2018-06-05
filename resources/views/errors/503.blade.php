3a8e1295555d4906b55e024ffa7be17e07e25bae
Eduardo Hernández <joredher30@gmail.com>
2018-06-05 17:50:48 -0500

    Modo en Mantenimiento

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.errors')
+
+@section('title', 'Modo Mantenimiento')
+
+@section('imagen')
+    <img src="{{ asset('imagenes/logo/logo_teach_2.png') }}" alt="503_Image">
+@endsection
+
+@section('numero', '503')
+
+@section('texto_uno', 'En Mantenimiento')
+
+@section('texto_dos', 'Pedimos disculpas por los incovenientes causados pero estamos trabajando en cosas interesantes.&iexcl;Pronto estaremos en línea de nuevo!')
